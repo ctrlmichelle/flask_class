7627930790baf8613f0d005d9c77aaa915a77390
ctrlmichelle <olachmichelle@gmail.com>
2026-05-05 18:06:55 +0300

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask,render_template,request,redirect,url_for,flash
 from database import get_products,get_sales,get_stock,insert_products,insert_sales,insert_stock,check_user_exists,get_users,insert_users
-
+from flask_bcrypt import Bcrypt
+from functools import wraps
 
 app = Flask(__name__)
+
+
+bcrypt = Bcrypt(app)
 
 app.secret_key = 'wowoyayywowy'
 
@@ -13,48 +17,21 @@ def home():
     return render_template('index.html')
 
 
+
+def login_required(f):
+    @wraps(f)
+    def protected(*args,**kwargs):
+        if 'email' not in session:
+            return redirect(url_for('login'))
+        return f(*args,**kwargs)
+    return protected
+
+
 # http://127.0.0.1:5000/products
 @app.route('/products')
 def products():
     products_data = get_products()
     return render_template('products.html',products_data = products_data)
-
-
-@app.route('/add_product',methods=['GET','POST'])
-def add_product():
-    if request.method == 'POST':
-        product_name = request.form['p_name']
-        buying_price = request.form['b_price']
-        selling_price = request.form['s_price']
-        new_product = (product_name,buying_price,selling_price)
-        insert_products(new_product)
-        flash("Product added successfully",'success')
-    return redirect(url_for('products'))
-
-
-@app.route('/sales')
-def sales():
-    sales_data = get_sales()
-    products_data = get_products()
-    return render_template('sales.html',sales_data = sales_data,products_data = products_data)
-
-
-@app.route('/add_sale',methods=['GET','POST'])
-def add_sale():
-    if request.method == 'POST':
-        product_id = request.form['p_id']
-        quantity = request.form['quantity']
-        new_sale = (product_id,quantity)
-        insert_sales(new_sale)
-        flash("Sale added successfully",'success')
-    return redirect(url_for('sales'))
-
-
-@app.route('/stock')
-def stock():
-    stock_data = get_stock()
-    products_data = get_products()
-    return render_template('stock.html',stock_data = stock_data,products_data = products_data)
 
 
 @app.route('/add_stock',methods=['GET','POST'])
@@ -68,34 +45,54 @@ def add_stock():
     return redirect(url_for('stock'))
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
     return render_template('dashboard.html',)
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('login.html')
-
-@app.route('/register')
-def register():
-    users_data = get_users()
-    return render_template('register.html',users_data = users_data)
-
-
-@app.route('/register_here',methods=['GET','POST'])
-def register_here():
     if request.method == 'POST':
         email = request.form['email']
-        if check_user_exists(email):
-            flash("User already exists",'danger')
-            return redirect(url_for('register'))
+        password = request.form['password']
+
+        existing_user = check_user_exists(email)
+        if not existing_user:
+            flash("User doesn't exist, please register",'danger')
         else:
-            full_name = request.form['f_name']
-            phone_number = request.form['phone_number']
-            password = request.form['password']
-            new_user = (full_name,email,phone_number,password)
+            if bcrypt.check_password_hash(existing_user[-1],password):
+                session['email'] = email
+                flash("Login successful",'success')
+                return redirect(url_for('dashboard'))
+            else:
+                flash("Password incorrect",'danger')
+    return render_template('login.html')
+
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        full_name = request.form['f_name']
+        email = request.form['email']
+        phone_number = request.form['phone_number']
+        password = request.form['password']
+        
+        existing_user = check_user_exists(email)
+        if not existing_user:
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            new_user = (full_name,email,phone_number,hashed_password)
             insert_users(new_user)
             flash("Account created successfully",'success')
-    return redirect(url_for('register'))
+        else:
+            flash("User already exists",'danger')
+    return render_template('register.html')
+
+#log out by removing email in session
+@app.route('/logout')
+def logout():
+    session.pop('email',None)
+    flash("Logged out successfully",'success')
+    return redirect(url_for('login'))
+
 
 
 app.run(debug=True)
